5da58a2cfad68103bab7ae66c0994ec0865f13a7
tahriik18 <tarique.mahtab.432@my.csun.edu>
2025-05-15 03:00:18 -0700

    Removed TeamId from Player model and updated database

---
+++ Migrations/20250515095638_RemoveTeamIdFromPlayer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerRoster.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTeamIdFromPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Players");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamId",
+                table: "Players",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
